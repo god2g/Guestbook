6ee8756d7eb2142ba58c8fb72fde9e13d9a2bd1d
god2g <370567301@qq.com>
2014-11-14 10:55:42 +0800

    Added Guestbook Model

---
+++ MvcGuestbook/MvcGuestbook/Models/Guestbook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcGuestbook.Models
+{
+    public class Guestbook
+    {
+        [Key]
+        public int No { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
